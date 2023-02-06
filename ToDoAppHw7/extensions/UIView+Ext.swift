95806f3cfe295d9b11326917fedc43f522d3428a
Berkay Tuncel <berkaytncl98@gmail.com>
2023-02-06 04:13:18 +0300

    Create UIView extension

---
+++ ToDoAppHw7/extensions/UIView+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Ext.swift
+//  ToDoAppHw7
+//
+//  Created by Berkay Tuncel on 6.02.2023.
+//
+
+import UIKit
+
+extension UIView {
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
